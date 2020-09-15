79ce10fac38b0449a426d705816cd4cac4b39879
Kirill Kotelevsky <yixwellet@gmail.com>
2020-09-15 23:31:57 +0400

    Настроил соединение с БД

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 import urllib.request
 import json
 import zipfile
-
+import psycopg2
+import xmlschema
+"""
 with urllib.request.urlopen('http://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo') as response:
     versions = json.loads(response.read())
 
@@ -17,3 +19,18 @@ for file in gar_zip.namelist():
             gar_zip.extract(file, '/media/owgrant/second/fias/gar_delta/')
     else:
         gar_zip.extract(file, '/media/owgrant/second/fias/gar_delta/')
+
+try:
+    conn = psycopg2.connect("dbname=geodata user=postgres password=07071907 host=localhost")
+except psycopg2.Error as e:
+    print(e)
+cursor = conn.cursor()
+cursor.execute("CREATE SCHEMA fias;")
+
+cursor.close()
+conn.commit()
+conn.close()
+"""
+
+schema = xmlschema.XMLSchema('/media/owgrant/second/fias/gar_delta/Schemas/AS_ADDR_OBJ_2_251_01_04_01_01.XSD')
+print(schema.attributes)
