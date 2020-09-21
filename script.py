09d380984c898185a1ae58bbd6d1ef7e492056e8
Kirill Kotelevsky <yixwellet@gmail.com>
2020-09-22 00:03:47 +0400

    реализовал перебор тегов и извлечение названий полей

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import zipfile
 import psycopg2
 import xmlschema
 from pprint import pprint
+import re
 """
 with urllib.request.urlopen('http://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo') as response:
     versions = json.loads(response.read())
@@ -33,18 +34,60 @@ conn.commit()
 conn.close()
 """
 
-schema = open(r'D:\\fias_gar_15092020\\Schemas\\AS_ADDR_OBJ_2_251_01_04_01_01.XSD', 'r', encoding='utf_8_sig')
-#schema = xmlschema.XMLSchema('/media/owgrant/second/fias/gar_delta/Schemas/AS_ADDR_OBJ_2_251_01_04_01_01.XSD')
-position = 0
-
+#schema = open(r'D:\\fias_gar_15092020\\Schemas\\AS_ADDR_OBJ_2_251_01_04_01_01.XSD', 'r', encoding='utf_8_sig')
+schema = open('/mnt/bad63750-d3a5-46f9-9477-a5075147cae2/fias/gar_delta/Schemas/AS_ADDR_OBJ_2_251_01_04_01_01.XSD','r',encoding='utf_8_sig')
 schemadict = {}
-
 schemastr = schema.read()
 
-while True:
-    x = schemastr.find('xs:', position)
-    currenttag = schemastr[x]
+def extractContent(xsd):
+    string = xsd.replace('\n','').replace('\t','')
+    return string[string.find('<xs:', 0):]
+
+"""
+def parser(string, dictionary=0):
+    
+    # Функция записывает индекс открывающего и закрывающего тегов, вызывает себя и передает себе текст между тегами
+    
+    if string.find('<xs:') == -1:
+        pass
+    else:
+        openTagStartIndex = string.find('<xs:')
+        openTagNameWithAttributes = string[openTagStartIndex:string.find('>')+1]
+        openTagNameClear = openTagNameWithAttributes[4:re.search(r'\ |>', openTagNameWithAttributes).start()]
+        closeTagName = '</xs:' + openTagNameClear
+        closeTagStartIndex = string.find(closeTagName)
+        tagContent = string[openTagStartIndex+len(openTagNameWithAttributes):closeTagStartIndex]
+        dictionary['tag'] = openTagNameClear
+        dictionary[openTagNameClear] = {}
+        parser(tagContent,dictionary[openTagNameClear],closeTagStartIndex + len(closeTagName)+1)
+"""
+openTags = []
+def diver(string, ellist, openTags=openTags):
+
+    if len(string) < 2:
+        return 'Search complete'
+
+    if string.find('<xs:') != -1 and string.find('</xs') != -1 and string.find('<xs:') < string.find('</xs'):
+        openTagStartIndex = string.find('<xs:')
+        openTagNameWithAttributes = string[openTagStartIndex:string.find('>')+1]
+        openTagNameClear = openTagNameWithAttributes[4:re.search(r'\ |>', openTagNameWithAttributes).start()]
+        openTags.append(openTagNameClear)
+        if openTagNameWithAttributes.find(' name=') != -1:
+            ellist.append(openTagNameWithAttributes[openTagNameWithAttributes.find(' name=')+7:openTagNameWithAttributes.find('"',openTagNameWithAttributes.find(' name=')+8)])
+        if openTagNameWithAttributes[-2] == '/':
+            openTags.pop()
+        diver(string[openTagStartIndex+len(openTagNameWithAttributes):],ellist)
+    else:
+        openTags.pop()
+        closeTagStartIndex = string.find('</xs:')
+        closeTagName = string[closeTagStartIndex:string.find('>')+1]
+        diver(string[closeTagStartIndex+len(closeTagName):],ellist)
 
 
-print(schemastr.find('<xs:', position))
-print(schemastr[131])
+elements = []
+
+string = extractContent(schemastr)
+diver(string, elements)
+print(elements)
+
+schema.close()
