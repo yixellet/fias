f6da928ee70b4d5987244fcf4670ca6b65d3a928
yixellet <yixwellet@gmail.com>
2021-02-26 17:39:20 +0400

    refactoring finished. Everything works

--- fillPgTables.py
+++ fillPgTables.py
@@ -1,49 +1,35 @@
 import os
 import xmlschema
 
+def insertIntoDb(XSDdirectory, XMLdirectory, file, item, schema, cursor, conn, param):
+    print('----- Таблица {0} заполняется... -----'.format(item[3:item.find('_20')]))
+    xs = xmlschema.XMLSchema('{0}/{1}'.format(XSDdirectory, file))
+    data = xs.to_dict('{0}/{1}'.format(XMLdirectory, item))
+    print(list(data.keys())[0])
+    if data != None:
+        for line in data[list(data.keys())[0]]:
+            columns = ''
+            values = ''
+            for pair in line.items():
+                columns = columns + '"{0}", '.format(pair[0][1:])
+                values = values + '\'{0}\', '.format(str(pair[1]))
+            cursor.execute('INSERT INTO {0}.{1} ({2}) VALUES ({3});' \
+                .format(schema, item[3:item.find('_20')] if (param or list(data.keys())[0] == 'ITEM') else list(data.keys())[0]+'S', columns[:-2], values[:-2]))
+            conn.commit()
+        print('----- ГОТОВО -----')
+
 def fillPgTables(XMLdirectory, XSDdirectory, cursor, conn, schema):
     for item in os.listdir(XMLdirectory):
         if item.find('.XML') != -1:
             if item.find('PARAMS') == -1:
                 for file in os.listdir(XSDdirectory):
-                    if file.find(item[:item.index('_2')]) != -1:
-                        xs = xmlschema.XMLSchema('{0}/{1}'.format(XSDdirectory, file))
-                        data = xs.to_dict('{0}/{1}'.format(XMLdirectory, item))
+                    if file[:file.index('_2')] == item[:item.index('_2')]:
+                        insertIntoDb(XSDdirectory, XMLdirectory, file, item, schema, cursor, conn, False)
             else:
                 for file in os.listdir(XSDdirectory):
                     if file.find('AS_PARAM_2') != -1:
-                        xs = xmlschema.XMLSchema('{0}/{1}'.format(XSDdirectory, file))
+                        insertIntoDb(XSDdirectory, XMLdirectory, file, item, schema, cursor, conn, True)
         
         if item == '30':
             fillPgTables(XMLdirectory + '/' + item, XSDdirectory, cursor, conn, schema)
-
-
-"""
-        if item.find('.XML') != -1:
-            if item[3:item.find('_2')] in list(parsedXSD.keys()):
-                data = parsedXSD[item[3:item.find('_2')]]['object'].to_dict(directory + '/' + item)
-                
-                if data != None:
-                    for line in data[list(data.keys())[0]]:
-                        columns = ''
-                        values = ''
-                        for key in line:
-                            columns = columns + '"' + str(key[1:]).lower() + '", '
-                            values = values + '\'' + str(line[key]) + '\', '
-                        print("INSERT INTO {3}.{0} ({1}) VALUES ({2});".format(item[3:item.find('_2')].lower(), columns[:-2], values[:-2], schema))
-                        cursor.execute("INSERT INTO {3}.{0} ({1}) VALUES ({2});".format(item[3:item.find('_2')].lower(), columns[:-2], values[:-2], schema))
-                    conn.commit()
-                
-            if item.find('PARAMS_2') != -1:
-                data = parsedXSD['PARAM']['object'].to_dict(directory + '/' + item)
-                if data != None:
-                    for line in data[list(data.keys())[0]]:
-                        columns = ''
-                        values = ''
-                        for key in line:
-                            columns = columns + '"' + str(key[1:]).lower() + '", '
-                            values = values + '\'' + str(line[key]) + '\', '
-                        print("INSERT INTO {3}.{0} ({1}) VALUES ({2});".format(item[3:item.find('_2')].lower(), columns[:-2], values[:-2], schema))
-                        cursor.execute("INSERT INTO {3}.{0} ({1}) VALUES ({2});".format(item[3:item.find('_2')].lower(), columns[:-2], values[:-2], schema))
-                    conn.commit()
-"""
+    print('--- Все таблицы заполнены ---')
