5ac9c0e7ebf10615dc6c6e3e0c7cd0815d7c91f2
yixellet <yixwellet@gmail.com>
2021-03-01 14:24:22 +0400

    реализую названия колонок в нижнем регистре

--- createIndexes.py
+++ createIndexes.py
@@ -11,25 +11,25 @@ def createIndexes(parsedXSD, conn, cursor, schemaName):
         if (schema['fileName'].find('TYPES') != -1 
             or schema['fileName'].find('DOCS') != -1):
             cursor.execute(
-                'CREATE INDEX {0}_id_idx ON {1}."{0}" USING btree ("ID")' \
+                'CREATE INDEX {0}_id_idx ON {1}."{0}" USING btree ("id")' \
                 .format((schema['fileName'][3:schema['fileName'].find('_2')]).lower(), schemaName)
             )
     cursor.execute('CREATE INDEX {1}_{2}_idx ON {0}."{1}" USING btree ("{2}")' \
-        .format(schemaName, 'change_history', 'CHANGEID')
+        .format(schemaName, 'change_history', 'changeid')
     )
 
     for table in tablesArray:
         cursor.execute(
             'CREATE INDEX {1}_{2}_idx ON {0}."{1}" USING btree ("{2}")' \
-            .format(schemaName, table, 'OBJECTID')
+            .format(schemaName, table, 'objectid')
         )
         cursor.execute(
             'CREATE INDEX {1}_{2}_idx ON {0}."{1}" USING btree ("{2}")' \
-            .format(schemaName, table, 'PREVID')
+            .format(schemaName, table, 'previd')
         )
         cursor.execute(
             'CREATE INDEX {1}_{2}_idx ON {0}."{1}" USING btree ("{2}")' \
-            .format(schemaName, table, 'NEXTID')
+            .format(schemaName, table, 'nextid')
         )
     conn.commit()
     print('--- Индексы созданы ---')
--- index.py
+++ index.py
@@ -5,16 +5,16 @@ from createPgTables import createPgTables
 from fillPgTables import fillPgTables
 from createIndexes import createIndexes
 
-(connection, cursor) = connectToDB(
-  DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_SCHEMA)
+# (connection, cursor) = connectToDB(
+#   DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_SCHEMA)
 
 parsedXSD = parseXsd(XSD_DIRECTORY)
+print(parsedXSD)
+# createPgTables(parsedXSD, connection, cursor, DB_SCHEMA)
 
-createPgTables(parsedXSD, connection, cursor, DB_SCHEMA)
+# fillPgTables(
+#   XML_DIRECTORY, XSD_DIRECTORY, cursor, connection, DB_SCHEMA, REGION_CODE)
 
-fillPgTables(
-  XML_DIRECTORY, XSD_DIRECTORY, cursor, connection, DB_SCHEMA, REGION_CODE)
+# createIndexes(parsedXSD, connection, cursor, DB_SCHEMA)
 
-createIndexes(parsedXSD, connection, cursor, DB_SCHEMA)
-
-connection.close()
+# connection.close()
